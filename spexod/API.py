40ab462696c6454e015d0d6b244c62d5a4645a58
Isaac Jaramillo <i_j40@txstate.edu>
2023-07-06 14:51:18 -0500

    Cleaned up some of the code and functionality.

--- spexod/API.py
+++ spexod/API.py
@@ -11,96 +11,125 @@ import pandas as pd
 import os
 import plotly.express as px
 
-server = 'https://spexodisks.com/api/'
+SERVER_URL = 'https://spexodisks.com/api/'
 
 
 def get_available_isotopologues() -> dict:
     """
     Returns a dictionary of available isotopologues and their properties
     """
-    url = server + 'available_isotopologues/'
-    response = requests.get(url)
-    return response.json()
+    url = SERVER_URL + 'available_isotopologues/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching isotopologues: {e}")
+        return None
 
 
 def get_params_and_units() -> dict:
     """
-    Returns a dictionary of available parameters and units
+    Returns a dictionary of available parameters and units.
     """
-    url = server + 'available_params_and_units/'
-    response = requests.get(url)
-    return response.json()
+    url = SERVER_URL + 'available_params_and_units/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching parameters and units: {e}")
+        return None
 
 
 def get_curated() -> dict:
     """
-    Returns a dictionary of curated data and corresponding handles
+    Returns a dictionary of curated data and corresponding handles.
     """
-    url = server + 'curated/'
-    response = requests.get(url)
-    return response.json()
+    url = SERVER_URL + 'curated/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching curated data: {e}")
+        return None
 
 
 def get_spectra() -> dict:
     """
-    Returns a dictionary of available spectra
+    Returns a dictionary of available spectra.
     """
-    url = server + 'spectra/'
-    response = requests.get(url)
-    return response.json()
+    url = SERVER_URL + 'spectra/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching spectra: {e}")
+        return None
 
 
 def get_star_aliases() -> dict:
     """
-    Returns a dictionary of star aliases and corresponding handles associated with alias
+    Returns a dictionary of star aliases and corresponding handles associated with alias.
     """
-    url = server + 'objectnamealiases/'
-    response = requests.get(url)
-    return response.json()
+    url = SERVER_URL + 'objectnamealiases/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching star aliases: {e}")
+        return None
 
 
 def find_spectra(alias=None) -> dict:
     """
-    Returns a dictionary of spectra associated with a given alias
+    Returns a dictionary of spectra associated with a given alias.
     """
     aliases = get_star_aliases()
-    aliasesandhandles = {}
-    possibleAliases = []
+    aliases_and_handles = {}
 
-    alias = alias.lower().strip()
     # Use a dictionary to map aliases to handles
-
     for i in aliases:
-        aliasesandhandles[i['alias'].lower().strip()] = i['spexodisks_handle'].lower().strip()
+        aliases_and_handles[i['alias'].lower().strip()] = i['spexodisks_handle'].lower().strip()
 
     if alias is None:
         print("Please provide an object name to search for.")
-        return
-    if alias not in aliasesandhandles.keys():
-        for i in aliasesandhandles.keys():
-            if alias in i:
-                possibleAliases.append(i)
-        return print(f"Found {len(possibleAliases)} possible aliases for {alias}: {possibleAliases} \n"
-                     f"Choose one of these aliases to search for and type it in verbatim.")
-    else:
-        return aliasesandhandles[alias]
+        return None
+
+    alias = alias.lower().strip()
+
+    if alias not in aliases_and_handles.keys():
+        possible_aliases = [i for i in aliases_and_handles.keys() if alias in i]
+        if len(possible_aliases) > 0:
+            print(f"Found {len(possible_aliases)} possible aliases for {alias}: {possible_aliases}")
+        else:
+            print(f"No matching aliases found for {alias}.")
+        return None
+
+    return aliases_and_handles[alias]
 
 
 def get_curated_data(handle: str) -> dict:
     """
-    Returns a dictionary of curated data for a given handle
+    Returns a dictionary of curated data for a given handle.
     """
     curated = get_curated()
 
-    for i in curated:
-        i['spexodisks_handle'] = i['spexodisks_handle'].lower().strip()
-        if i['spexodisks_handle'] == handle.lower().strip():
-            return i
+    handle = handle.lower().strip()
+
+    for data in curated:
+        if data['spexodisks_handle'].lower().strip() == handle:
+            return data
+
+    return None
 
 
 def get_all_spectra_handles(spexodisks_handle: str) -> list:
     """
-    Returns a list of all spectra handles for a given star/alias
+    Returns a list of all spectra handles for a given star/alias.
     """
     spectra = get_spectra()
     handles = []
@@ -112,23 +141,50 @@ def get_all_spectra_handles(spexodisks_handle: str) -> list:
 
 
 def get_wavelengths(handle: str) -> dict:
-    """ Returns a dictionary of wavelengths for a given handle"""
-    url = server + handle.lower() + '/'
-    response = requests.get(url)
-    return response.json()['wavelength_um']
+    """
+    Returns a dictionary of wavelengths for a given handle.
+    """
+    url = SERVER_URL + handle.lower() + '/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()['wavelength_um']
+    except requests.RequestException as e:
+        print(f"Error fetching wavelengths for handle '{handle}': {e}")
+        return None
 
 
 def get_fluxes(handle: str) -> tuple:
-    """ Returns a tuple of fluxes and flux errors for a given handle"""
-    url = server + handle.lower() + '/'
-    response = requests.get(url)
-    return response.json()['flux'], response.json()['flux_error']
+    """
+    Returns a tuple of fluxes and flux errors for a given handle.
+    """
+    url = SERVER_URL + handle.lower() + '/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        fluxes = data.get('flux')
+        flux_errors = data.get('flux_error')
+        return fluxes, flux_errors
+    except requests.RequestException as e:
+        print(f"Error fetching fluxes for handle '{handle}': {e}")
+        return None, None
 
 
 def get_stars_from_file(filename: str) -> list:
-    """ Returns a list of stars from a given file"""
-    df = pd.read_csv(filename, sep=",")
-    return df.columns.tolist()
+    """
+    Returns a list of stars from a given file.
+    """
+    try:
+        df = pd.read_csv(filename, sep=",")
+        return df.columns.tolist()
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+    except pd.errors.EmptyDataError:
+        print(f"File '{filename}' is empty.")
+    except pd.errors.ParserError:
+        print(f"Error parsing file '{filename}'.")
+    return []
 
 
 def create_spectra_file(stars: list) -> None:
@@ -140,57 +196,74 @@ def create_spectra_file(stars: list) -> None:
         star_spectra[star] = get_all_spectra_handles(spectra)
 
     # Create a folder for each star
-    for k, v in star_spectra.items():
+    for star, spectra_list in star_spectra.items():
         # Create a folder for each key
-        if not os.path.exists(k):
-            os.makedirs(k)
-        os.chdir(k)
+        if not os.path.exists(star):
+            os.makedirs(star)
+        os.chdir(star)
         # For each value, find from API and write to csv
-        for i in v:
-            wavelengths = get_wavelengths(i['spectrum_handle'])
-            fluxes, flux_errors = get_fluxes(i['spectrum_handle'])
-            print(f"Writing {i['spexodisks_handle']}.csv")
-            print(f"Found {len(wavelengths)} wavelengths and {len(fluxes)} fluxes for {i['spexodisks_handle']}")
+        for spectrum in spectra_list:
+            wavelengths = get_wavelengths(spectrum['spectrum_handle'])
+            fluxes, flux_errors = get_fluxes(spectrum['spectrum_handle'])
+            print(f"Writing {spectrum['spexodisks_handle']}.csv")
+            print(f"Found {len(wavelengths)} wavelengths and {len(fluxes)} fluxes for {spectrum['spexodisks_handle']}")
             df = pd.DataFrame({'wavelength_um': wavelengths, 'flux': fluxes, 'flux_error': flux_errors})
-            df.to_csv(f"{i['spectrum_handle']}.csv", index=False)
+            df.to_csv(f"{spectrum['spectrum_handle']}.csv", index=False)
         # Create a csv file for each key
-        df = pd.DataFrame(v)
-        df.to_csv(f"{k}.csv", index=False)
+        df = pd.DataFrame(spectra_list)
+        df.to_csv(f"{star}.csv", index=False)
         os.chdir("..")
 
-
-def login():
+def login() -> dict:
     """
-    Logs into the spexodisks database
+    Logs into the SpExoDisks database.
     """
     access = False
     while not access:
         username = input("Please enter your username: ")
         password = input("Please enter your password: ")
-        url = server + 'users/token/'
-        response = requests.post(url, json={'email': username, 'password': password})
-        if response.status_code == 200:
-            print("Login successful.")
-            access = True
-            return response.json()
-        else:
-            print("Login failed. Please try again.")
+        url = SERVER_URL + 'users/token/'
+        try:
+            response = requests.post(url, json={'email': username, 'password': password})
+            response.raise_for_status()
+            if response.status_code == 200:
+                print("Login successful.")
+                access = True
+                return response.json()
+            else:
+                print("Login failed. Please try again.")
+        except requests.RequestException as e:
+            print(f"Error during login: {e}")
+            return None
 
 
 def download_spectrum(spectra: List) -> None:
     """
-    Downloads a spectrum from the spexodisks database
+    Downloads spectra from the SpExoDisks database.
     """
-    accessToken = login()['access']
-    url = server + 'datadownload/?spectra='
-    for i in spectra:
-        url += i + '%'
-    r = requests.get(url, headers={'Authorization': 'Bearer ' + accessToken})
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall("spectra")  # extract to folder
+    access_token = login().get('access')
+    if not access_token:
+        print("Login failed. Cannot download spectra.")
+        return
+
+    url = SERVER_URL + 'datadownload/?spectra='
+    for spectrum in spectra:
+        url += spectrum + '%'
+
+    headers = {'Authorization': 'Bearer ' + access_token}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        zip_file = zipfile.ZipFile(io.BytesIO(response.content))
+        zip_file.extractall("Spectra")
+        print("Spectra downloaded successfully.")
+    except requests.RequestException as e:
+        print(f"Error during spectrum download: {e}")
 
 
-def plot_spectra(wavelength, flux):
+def plot_spectra(wavelength, flux) -> None:
     """
     Plots a spectrum
     """
@@ -211,8 +284,7 @@ if __name__ == "__main__":
     # print(get_curated_data('leftsqbracketc91rightsqbracket_irs_1'))
     # print(get_all_spectra_handles('IRAS_08470minus4321'))
     # print(get_wavelengths('crires_2254nm_2356nm_IRAS_08470minus4321'))
-    # print(get_fluxes('crires_2254nm_2356nm_IRAS_08470minus4321')[1])
-    # print(get_flux_errors('crires_2254nm_2356nm_IRAS_08470minus4321'))
+    # print(get_fluxes('crires_2254nm_2356nm_IRAS_08470minus4321'))
     # print(get_stars_from_file('test.txt'))
     # stars = get_stars_from_file('test.txt')
     # create_spectra_file(stars)
